5bc4187995d0581a9cd089cde756db744a678933
xiaocai <1766607952@qq.com>
2021-06-04 11:50:38 +0800

    适配

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,16 @@
       黑马头条
       <button>乞丐按钮</button>
       <van-button type="primary">主要按钮</van-button>
+      <van-button type="primary">主要按钮</van-button>
+      <van-button type="primary">主要按钮</van-button>
+      <van-button type="primary">主要按钮</van-button>
     </div>
     <router-view />
   </div>
 </template>
+
+<style scoped>
+.nav {
+  font-size: 20px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -4,9 +4,12 @@ import router from './router'
 import store from './store'
 
 import Vant from 'vant' // 组件库
-import 'vant/lib/index.css' // 样式
+import 'vant/lib/index.css' // 引入
 
-Vue.use(Vant) // 引入
+// 它会根据的手机尺寸来调整rem的基准值：html标签上的font-size。
+import 'amfe-flexible' // 样式
+
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 

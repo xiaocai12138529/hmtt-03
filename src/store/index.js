fe316c0655e66db385d02387cb4077b76da9e8bd
xiaocai <1766607952@qq.com>
2021-06-09 17:01:14 +0800

    功能:持久化token

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import { login } from '@/api/user.js'
 import user from './modules/user'
+import { saveToken } from '@/utils/storage.js'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
@@ -21,6 +22,7 @@ export default new Vuex.Store({
         const res = await login(userInfo)
         // 保存token
         context.commit('setToken', res.data.data)
+        saveToken(res.data.data)
       } catch (err) {
         console.log(err)
         throw new Error(err)
